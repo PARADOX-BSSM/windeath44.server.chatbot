8db06320f7fa446588face0752cb3bf78c34436e
pdh0128 <pdh0128a@gmail.com>
2025-10-06 00:09:53 +0900

    feat :: 추모관 캐릭터 정보 조회

--- api/routers/dit.py
+++ api/routers/dit.py
@@ -1,14 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from app.dit.service import dit_service
 from api.schemas.common.response.base_response import BaseResponse
+from core.grpcs.client.chatbot_grpc_client import ChatbotGrpcClient
+from core.grpcs.deps.chatbot_stub_dep import chatbot_stub_dep
 
 router = APIRouter(prefix="/chatbots/dits", tags=["dit"])
 
 @router.post("")
-async def write_tribute():
+async def write_tribute(
+        chatbot_grpc_client : ChatbotGrpcClient = Depends(chatbot_stub_dep)
+):
     # character id 랜덤이라 가정
     # memorial id 랜덤이라 가정
     character_id = 1
-    memorial_id = 103
-    await dit_service.write_memorial(character_id, memorial_id)
+    memorial_id = 1
+    await dit_service.write_memorial(character_id, memorial_id, chatbot_grpc_client)
     return BaseResponse(message="")
--- app/dit/service/dit_service.py
+++ app/dit/service/dit_service.py
@@ -6,32 +6,31 @@ from app.chatbot.service import chatbot_service
 import os
 
 from app.dit.util import memorial_http_util
+from core.grpcs.client.chatbot_grpc_client import ChatbotGrpcClient
+
 load_dotenv()
 
 WRITE_MEMORIAL_PROMPT = """
 지금 당신은 팬이 만든 추모관을 보고 댓글을 작성하려 합니다.
 
+[추모관 캐릭터 정보]
+이름 : {name}
+생전에 했던 명언 : {saying}
+출연한 애니메이션 이름 : {anime_name}
+
 [추모관 내용]
-{contentdnjanjwdadn}
+{content}
 
 [받은 절 횟수]
-{bowedmajondai}
+{bow}
 
 위 내용을 읽고, 당신의 감정이 자연스럽게 추모 댓글을 작성하세요.  
 """
 
-memorial_domain = os.getenv("MEMORIAL_DOMAIN")
-
-# characterId,
-# bowCount,
-# memorialCommitId,
-# content,
-# createdAt,
-# updatedAt
-
-
-async def write_memorial(character_id : int, memorial_id : int):
-    # 1. 추모관 글 조회
+async def write_memorial(character_id : int, memorial_id : int, chatbot_grpc_client : ChatbotGrpcClient):
+    # 1. 추모관 조회
+    # 1.1 추모관 글 조회
+    # 1.2 추모관 캐릭터 조회
     # 2. 추모관 글을 토대로 프롬프트 구성
     # 3. 챗봇 응답 받기
     # 4. 글 작성 요청 보내기
@@ -39,11 +38,22 @@ async def write_memorial(character_id : int, memorial_id : int):
     # 추모관 글 조회
     memorial_content = await memorial_http_util.get_memorial_content(memorial_id=memorial_id)
 
+    # 추모관 캐릭터 조회
+    memorial_character = await chatbot_grpc_client.get_character(character_id=memorial_content["characterId"])
+    # animeId = 1
+    # animeName = 2
+    # name = 3
+    # content = 4
+    # state = 5
+
     # prompt 구성
     prompt = (
         WRITE_MEMORIAL_PROMPT
-        .replace("{contentdnjanjwdadn}", memorial_content["content"])
-        .replace("{bowedmajondai}", str(memorial_content["bowCount"]))
+        .replace("{content}", memorial_content["content"])
+        .replace("{bow}", str(memorial_content["bowCount"]))
+        .replace("{name}", memorial_character.name)
+        .replace("{anime_name}", memorial_character.animeName)
+        .replace("{saying}", memorial_character.content)
     )
     print(prompt)
     chat_request = ChatRequest(
