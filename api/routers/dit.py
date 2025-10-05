eb27d35b832e811ac7df73bb34545d9f865a94ef
pdh0128 <pdh0128a@gmail.com>
2025-10-05 23:39:58 +0900

    feat :: prompt 구성

--- api/routers/dit.py
+++ api/routers/dit.py
@@ -2,9 +2,9 @@ from fastapi import APIRouter
 from app.dit.service import dit_service
 from api.schemas.common.response.base_response import BaseResponse
 
-router = APIRouter(prefix="/dits", tags=["dit"])
+router = APIRouter(prefix="/chatbots/dits", tags=["dit"])
 
-@router.post
+@router.post("")
 async def write_tribute():
     # character id 랜덤이라 가정
     # memorial id 랜덤이라 가정
--- app/dit/service/dit_service.py
+++ app/dit/service/dit_service.py
@@ -8,7 +8,17 @@ import os
 from app.dit.util import memorial_http_util
 load_dotenv()
 
-WRITE_MEMORIAL_PROMPT = "추모관에 댓글 작성할거야."
+WRITE_MEMORIAL_PROMPT = """
+지금 당신은 팬이 만든 추모관을 보고 댓글을 작성하려 합니다.
+
+[추모관 내용]
+{contentdnjanjwdadn}
+
+[받은 절 횟수]
+{bowedmajondai}
+
+위 내용을 읽고, 당신의 감정이 자연스럽게 추모 댓글을 작성하세요.  
+"""
 
 memorial_domain = os.getenv("MEMORIAL_DOMAIN")
 
@@ -30,10 +40,15 @@ async def write_memorial(character_id : int, memorial_id : int):
     memorial_content = await memorial_http_util.get_memorial_content(memorial_id=memorial_id)
 
     # prompt 구성
-    chat_request = ChatRequest(
-        content=WRITE_MEMORIAL_PROMPT
+    prompt = (
+        WRITE_MEMORIAL_PROMPT
+        .replace("{contentdnjanjwdadn}", memorial_content["content"])
+        .replace("{bowedmajondai}", str(memorial_content["bowCount"]))
     )
-
+    print(prompt)
+    chat_request = ChatRequest(
+        content=prompt
+    )
 
     # chatbot 응답 받기
     chatbot_response = await chatbot_service.chat(chatbot_id=character_id, chat_request=chat_request, user_id=str(memorial_id))
